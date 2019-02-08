53412157eb936554a11238cc99ff3379f7335297
Justin <jgran1@unh.newhaven.edu>
2019-02-08 18:09:00 -0500

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,21 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'First test todo'
+}, {
+	text: 'Second test todo'
+}];
+
 // Testign lifecycle method.
 // beforeEach is going to let us run some code before every single test case.
 // In this case, we will use beforeEach to set up the database in a way that's useful.
 // This function will run before every test case and it's only going to move on to the test case
 // once we called done, which means we can do something asynchronous inside of here.
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -29,7 +37,7 @@ describe('POST /todos', () => {
 					return done(err);
 				}
 
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -48,9 +56,21 @@ describe('POST /todos', () => {
 				}
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((e) => done(e));
 			});
+	});
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 });
