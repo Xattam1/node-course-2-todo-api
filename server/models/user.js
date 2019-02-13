051847920f2e31c84b2a3d871bf8d3f6fe96d34f
Justin <jgran1@unh.newhaven.edu>
2019-02-13 14:23:48 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -40,7 +40,7 @@ UserSchema.methods.toJSON = function () {
 };
 
 UserSchema.methods.generateAuthToken = function () {
-	var user = this;
+	var user = this;		// Instance
 	var access = 'auth';
 	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
@@ -48,6 +48,23 @@ UserSchema.methods.generateAuthToken = function () {
 
 	return user.save().then(() => {
 		return token;
+	});
+};
+
+UserSchema.statics.findByToken = function (token) {
+	var User = this;		// Model
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,		// Queries a nested document -> tokens.token.
+		'tokens.access': 'auth'
 	});
 };
 
