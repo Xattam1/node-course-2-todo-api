8f0c0f3bba7b291eb19c3546d5c51e36bacde062
Justin <jgran1@unh.newhaven.edu>
2019-02-15 22:46:45 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -284,7 +284,7 @@ describe('POST /users/login', () => {
 			.post('/users/login')
 			.send({
 				email: users[1].email,
-				password: 'abc123'
+				password: users[1].password + '1'
 			})
 			.expect(400)
 			.expect((res) => {
@@ -296,6 +296,26 @@ describe('POST /users/login', () => {
 				}
 
 				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+});
+
+describe('DELETE /users/me/token', () => {
+	it('should remove auth token on logout', (done) => {
+		request(app)
+			.delete('/users/me/token')
+			.set('x-auth', users[0].tokens[0].token)
+			.send()
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findById(users[0]._id).then((user) => {
 					expect(user.tokens.length).toBe(0);
 					done();
 				}).catch((e) => done(e));
